22d92d9fee4c8eebea815f2c92a36a5bce68ad00
akbars95 <mynzat.dmitrii@gmail.com>
2015-11-06 18:32:05 +0200

    -add new java7 project and first question

---
+++ java7/src/com/mtsmda/javaQuestions/inheritancePrivateConstructor/A.java
@@ -0,0 +1,20 @@
+package com.mtsmda.javaQuestions.inheritancePrivateConstructor;
+/**
+ * Inheritance with parent class have private constructor
+ * */
+public class A {
+	
+	private String name;
+	
+	private A() {
+		
+	}
+	
+}
+
+
+class B extends A{
+	public B() {
+		
+	}
+}
